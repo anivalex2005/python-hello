780374a219aff56f218dd1517185cf9351cdb3e3
anivalex2005 <63561854+anivalex2005@users.noreply.github.com>
2021-10-06 19:26:58 -0500

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "daddy"
-    message = "Fuck me, " + name + "!\n"
+    message = "hello, " + name + "!\n"
     return Response(message)
 
 if __name__ == '__main__':
